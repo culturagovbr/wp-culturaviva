7e495b585068d714131727840289d04afe87a49d
Fernão Lopes <fernao@riseup.net>
2015-11-05 11:45:47 -0200

    fixed #23; fixed #21

--- wp-content/themes/redeculturaviva/template-parts/content.php
+++ wp-content/themes/redeculturaviva/template-parts/content.php
@@ -46,13 +46,13 @@ if ( has_post_thumbnail() )
 	</div>
 	<div class="archive-col2">
 		<div class="entry-content">
-			<?php
-				the_content( sprintf(
+			<?php the_excerpt(); ?>
+            <a href="<?php the_permalink(); ?>"><?php print( sprintf(
 					/* translators: %s: Name of current post. */
 					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'rede-cultura-viva' ), array( 'span' => array( 'class' => array() ) ) ),
 					the_title( '<span class="screen-reader-text">"', '"</span>', false )
 				) );
-			?>
+			?></a>
 	
 			<?php
 				wp_link_pages( array(
