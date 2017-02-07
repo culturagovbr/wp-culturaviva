09c2f5134563fb605862900ba28d24862477ad6f
Avaloni <avaloni@users.noreply.github.com>
2017-02-07 15:37:19 -0200

    atualização wp 4.7.2

--- wp-content/themes/redeculturaviva/inc/widgets/events_widget_config.php
+++ wp-content/themes/redeculturaviva/inc/widgets/events_widget_config.php
@@ -7,7 +7,7 @@ class Mapas_Culturais_Events_Widget_Config
 {
 	public $_pghost = 'localhost';
 	public $_pgport = '5432';
-	public $_pgdb = 'mapas';
-	public $_pguser = 'mapas';
-	public $_pgpasswd = 'mapas';
+	public $_pgdb = 'mapasculturais';
+	public $_pguser = 'mapasculturais';
+	public $_pgpasswd = 'mapasculturais';
 }
